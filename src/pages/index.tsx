328fdf46fd304efefecab27ea0e5cb830a5be0cd
Francis Eshun <eshunfrancisboateng@gmail.com>
2023-02-28 15:07:43 +0000

    fix: fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,17 @@ import {
   ADJECTIVES1,
   ADJECTIVES2,
   ADJECTIVES3,
+  ADVERBS1,
+  ADVERBS3,
+  NOUNS1,
+  NOUNS2,
+  NOUNS3,
+  PREPOSITIONS1,
+  PREPOSITIONS2,
+  PREPOSITIONS3,
+  VERBS1,
+  VERBS2,
+  VERBS3,
 } from '@/data/finalwords/adjectives1';
 
 import Layout from '@/components/layout/Layout';
@@ -21,14 +32,46 @@ export default function HomePage() {
     to: string;
   }>();
 
-  const [list, setList] = useState<any>();
+  const [list, setList] = useState<string>();
 
-  //Handle list change
+  //Handle list change. Get the data from local storage
   React.useEffect(() => {
+    const adjectives1 = localStorage.getItem('adjectives1');
+    const adjectives2 = localStorage.getItem('adjectives2');
+    const adjectives3 = localStorage.getItem('adjectives3');
+    const adverb1 = localStorage.getItem('adverb1');
+    const adverb2 = localStorage.getItem('adverb2');
+    const adverb3 = localStorage.getItem('adverb3');
+    const noun1 = localStorage.getItem('noun1');
+    const noun2 = localStorage.getItem('noun2');
+    const noun3 = localStorage.getItem('noun3');
+    const preposition1 = localStorage.getItem('preposition1');
+    const preposition2 = localStorage.getItem('preposition2');
+    const preposition3 = localStorage.getItem('preposition3');
+    const verb1 = localStorage.getItem('verb1');
+    const verb2 = localStorage.getItem('verb2');
+    const verb3 = localStorage.getItem('verb3');
+
     if (list == 'adjective') {
-      setSyllable1(ADJECTIVES1);
-      setSyllable2(ADJECTIVES2);
-      setSyllable3(ADJECTIVES3);
+      setSyllable1(JSON.parse(adjectives1 ?? '[]'));
+      setSyllable2(JSON.parse(adjectives2 ?? '[]'));
+      setSyllable3(JSON.parse(adjectives3 ?? '[]'));
+    } else if (list == 'adverb') {
+      setSyllable1(JSON.parse(adverb1 ?? '[]'));
+      setSyllable2(JSON.parse(adverb2 ?? '[]'));
+      setSyllable3(JSON.parse(adverb3 ?? '[]'));
+    } else if (list == 'noun') {
+      setSyllable1(JSON.parse(noun1 ?? '[]'));
+      setSyllable2(JSON.parse(noun2 ?? '[]'));
+      setSyllable3(JSON.parse(noun3 ?? '[]'));
+    } else if (list == 'preposition') {
+      setSyllable1(JSON.parse(preposition1 ?? '[]'));
+      setSyllable2(JSON.parse(preposition2 ?? '[]'));
+      setSyllable3(JSON.parse(preposition3 ?? '[]'));
+    } else if (list == 'verb') {
+      setSyllable1(JSON.parse(verb1 ?? '[]'));
+      setSyllable2(JSON.parse(verb2 ?? '[]'));
+      setSyllable3(JSON.parse(verb3 ?? '[]'));
     } else {
       setSyllable1([]);
       setSyllable2([]);
@@ -36,9 +79,30 @@ export default function HomePage() {
     }
   }, [list]);
 
-  function moveWord(word: string, to: '1' | '2' | '3') {
-    //
-  }
+  //When there is a change in syllable1, syllable2, syllable3, save the data to local storage
+  React.useEffect(() => {
+    if (list == 'adjective') {
+      localStorage.setItem('adjectives1', JSON.stringify(syllable1));
+      localStorage.setItem('adjectives2', JSON.stringify(syllable2));
+      localStorage.setItem('adjectives3', JSON.stringify(syllable3));
+    } else if (list == 'adverb') {
+      localStorage.setItem('adverb1', JSON.stringify(syllable1));
+      localStorage.setItem('adverb2', JSON.stringify(syllable2));
+      localStorage.setItem('adverb3', JSON.stringify(syllable3));
+    } else if (list == 'noun') {
+      localStorage.setItem('noun1', JSON.stringify(syllable1));
+      localStorage.setItem('noun2', JSON.stringify(syllable2));
+      localStorage.setItem('noun3', JSON.stringify(syllable3));
+    } else if (list == 'preposition') {
+      localStorage.setItem('preposition1', JSON.stringify(syllable1));
+      localStorage.setItem('preposition2', JSON.stringify(syllable2));
+      localStorage.setItem('preposition3', JSON.stringify(syllable3));
+    } else if (list == 'verb') {
+      localStorage.setItem('verb1', JSON.stringify(syllable1));
+      localStorage.setItem('verb2', JSON.stringify(syllable2));
+      localStorage.setItem('verb3', JSON.stringify(syllable3));
+    }
+  }, [syllable1, syllable2, syllable3]);
 
   return (
     <Layout>
@@ -118,6 +182,88 @@ export default function HomePage() {
                   }}
                 >
                   Undo
+                </button>
+              </div>
+
+              {/* Button at the top left to load the initial data to local storage */}
+              <div className='fixed top-0 left-0 mt-5 ml-5'>
+                <button
+                  className='rounded bg-slate-100 px-3 py-1'
+                  onClick={() => {
+                    localStorage.setItem(
+                      'adjectives1',
+                      JSON.stringify(ADJECTIVES1)
+                    );
+                    localStorage.setItem(
+                      'adjectives2',
+                      JSON.stringify(ADJECTIVES2)
+                    );
+                    localStorage.setItem(
+                      'adjectives3',
+                      JSON.stringify(ADJECTIVES3)
+                    );
+                    localStorage.setItem('adverb1', JSON.stringify(ADVERBS1));
+                    localStorage.setItem(
+                      'adverb2',
+                      JSON.stringify(ADJECTIVES2)
+                    );
+                    localStorage.setItem('adverb3', JSON.stringify(ADVERBS3));
+                    localStorage.setItem('noun1', JSON.stringify(NOUNS1));
+                    localStorage.setItem('noun2', JSON.stringify(NOUNS2));
+                    localStorage.setItem('noun3', JSON.stringify(NOUNS3));
+                    localStorage.setItem(
+                      'preposition1',
+                      JSON.stringify(PREPOSITIONS1)
+                    );
+                    localStorage.setItem(
+                      'preposition2',
+                      JSON.stringify(PREPOSITIONS2)
+                    );
+                    localStorage.setItem(
+                      'preposition3',
+                      JSON.stringify(PREPOSITIONS3)
+                    );
+                    localStorage.setItem('verb1', JSON.stringify(VERBS1));
+                    localStorage.setItem('verb2', JSON.stringify(VERBS2));
+                    localStorage.setItem('verb3', JSON.stringify(VERBS3));
+
+                    setList('adjective');
+                  }}
+                >
+                  Load Initial Data
+                </button>
+              </div>
+
+              {/* Button at the top right to save all the data from the local storage into an object-array item like this: adjective:[] */}
+              <div className='fixed top-0 right-0 mt-20 mr-5'>
+                <button
+                  className='rounded bg-slate-100 px-3 py-1'
+                  onClick={() => {
+                    const data = {
+                      adjective1: localStorage.getItem('adjectives1'),
+                      adjective2: localStorage.getItem('adjectives2'),
+                      adjective3: localStorage.getItem('adjectives3'),
+                      adverb1: localStorage.getItem('adverb1'),
+                      adverb2: localStorage.getItem('adverb2'),
+                      adverb3: localStorage.getItem('adverb3'),
+                      noun1: localStorage.getItem('noun1'),
+                      noun2: localStorage.getItem('noun2'),
+                      noun3: localStorage.getItem('noun3'),
+                      preposition1: localStorage.getItem('preposition1'),
+                      preposition2: localStorage.getItem('preposition2'),
+                      preposition3: localStorage.getItem('preposition3'),
+                      verb1: localStorage.getItem('verb1'),
+                      verb2: localStorage.getItem('verb2'),
+                      verb3: localStorage.getItem('verb3'),
+                    };
+
+                    console.log(data);
+
+                    //copy to clipboard
+                    navigator.clipboard.writeText(JSON.stringify(data));
+                  }}
+                >
+                  Finalize Data
                 </button>
               </div>
             </div>
